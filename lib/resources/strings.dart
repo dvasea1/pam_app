ce2526a722f99bbd723c7b51537fbecc7e6ed3b1
Vasile Barbaros <vasile.barbaros@ebs-integrator.com>
2022-10-11 14:29:18 +0300

    removed all

--- lib/resources/strings.dart
+++
@@ -1,4 +0,0 @@
-class Strings {
-  static String helloUser  = 'Hello';
-  static String findAndTrackEat  = 'Find, track and eat heathy food.';
-}
--- lib/resources/svg_assets.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_svg/flutter_svg.dart';
-
-class SvgAsset {
-  static SvgPicture cake = SvgPicture.asset('resources/svg/cake.svg',);
-
-  static SvgPicture vegetables({double? width}) => SvgPicture.asset('resources/svg/vegetables.svg', width: width);
-}
